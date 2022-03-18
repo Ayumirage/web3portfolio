057d5d94f5a2afd30aa83c95779246ae349fc79a
vm.vdst <vm.vdst@gmail.com>
2022-03-18 11:19:30 +0100

    rel noreferrer

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -78,7 +78,7 @@ const Work = () => {
                 }}
                 className="app__work-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="norefferer">
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
